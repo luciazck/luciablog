d34dedb823b6e44b0f4c62ca1bedeb953cb09037
Lucia <lucia.zacikova@gmail.com>
2017-03-12 12:52:34 +0100

    files

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,9 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.
-
-files',
+    'django.contrib.files',
     'blog',
 )
 
