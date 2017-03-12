3f5825f7a7882bfdae0a546b24a4439ec1c80730
Lucia <lucia.zacikova@gmail.com>
2017-03-12 12:50:44 +0100

    static

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.
+
+files',
     'blog',
 )
 
@@ -101,4 +103,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIS_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
